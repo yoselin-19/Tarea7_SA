d80bf30bb27972d274fb6b9249180666812705d0
yoselin-19 <annelice119@gmail.com>
2020-04-11 02:40:43 -0600

    Creando la pagina a mostrar

--- NODEJS/api/src/index.js
+++ NODEJS/api/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const port = 3000
 
-app.get('/', (req, res) => res.send('<h1>Tarea 7</h1>'))
-app.get('/vista1', (req, res) => res.send('<h1>Vista1</h1>'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/home.html'));
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
